3b6edac89a0a5b584aba2c58b35cbed8191adb95
Diego Negrette <diegonegrette42@gmail.com>
2023-09-08 14:09:27 -0400

    Added emulators config

--- src/app/shared/models/notification.model.ts
+++ src/app/shared/models/notification.model.ts
@@ -9,10 +9,49 @@ export interface Notification {
     type: 'alert' | 'activity' | 'request'
 }
 
-// export class Notification {
-//     constructor(
-//         public
-//     ) {
+// export interface NotificationJson {
+//     id: string,
+//     readByUsers: [],
+//     readByAdmin: boolean,
+//     message: string,
+//     date: number, // timestamp
+//     userId: string,
+//     empresaId: string,
+//     type: 'alert' | 'activity' | 'request'
+// }
 
+// export class Notification {
+
+//     public static TYPE_ALERT: string = 'alert'
+//     public static TYPE_ACTIVITY: string = 'activity'
+//     public static TYPE_REQUEST: string = 'request'
+
+//     public id: string
+//     public readByUsers: []
+//     public readByAdmin: boolean
+//     public message: string
+//     public date: number // timestamp
+//     public userId: string
+//     public empresaId: string
+//     public type: 'alert' | 'activity' | 'request'
+
+//     constructor(
+//         id: string,
+//         readByUsers: [],
+//         readByAdmin: boolean,
+//         message: string,
+//         date: number, // timestamp
+//         userId: string,
+//         empresaId: string,
+//         type: string
+//     ) {
+//         this.id = 
+//         this.readByUsers = 
+//         this.readByAdmin = 
+//         this.message = 
+//         this.date = 
+//         this.userId = 
+//         this.empresaId = 
+//         this.type = 
 //     }
 // }
