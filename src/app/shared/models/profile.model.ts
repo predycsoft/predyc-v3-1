664dbf0e6d7705de5e5562010e99310580c63069
Diego Negrette <diegonegrette42@gmail.com>
2023-09-21 11:47:41 -0400

    profile service added

--- src/app/shared/models/profile.model.ts
+++ src/app/shared/models/profile.model.ts
@@ -1,6 +1,32 @@
 
-export interface Profile {
+export interface ProfileJson {
     id: string,
     name: string,
     studyPlan: []
+}
+
+export class Profile {
+    public static collection: string = 'profile'
+
+    constructor(
+        public id: string,
+        public name: string,
+        public studyPlan: []
+    ) {}
+
+    public static fromJson(profileJson: ProfileJson): Profile {
+    return new Profile(
+        profileJson.id,
+        profileJson.name,
+        profileJson.studyPlan
+        )
+    }
+      
+    toJson(): ProfileJson {
+        return {
+        id: this.id,
+        name: this.name,
+        studyPlan: this.studyPlan
+        }
+    }
 }
