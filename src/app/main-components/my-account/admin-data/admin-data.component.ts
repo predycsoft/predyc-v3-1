aa3ec66c8329dc739cf7a2507b198158e83a1ca2
Diego Negrette <diegonegrette42@gmail.com>
2023-09-25 13:28:55 -0400

    Added functionality to admin forms

--- src/app/main-components/my-account/admin-data/admin-data.component.ts
+++ src/app/main-components/my-account/admin-data/admin-data.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AdminDataComponent {
 
+  presentationData = {}
+  infoData = {}
+
+
+  onAdminPresentationChangeHandler(data) {
+    try {
+      console.log("Admin presentation data")
+      console.log(data)
+      this.presentationData = data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  onAdminInfoChangeHandler(data) {
+    try {
+      console.log("Admin info data")
+      console.log(data)
+      this.infoData = data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  onUpdate() {
+    const adminNewData = {...this.presentationData, ...this.infoData}
+    console.log("adminNewData")
+    console.log(adminNewData)
+  }
+
+
 }
--- src/app/main-components/my-account/admin-data/admin-info-form/admin-info-form.component.ts
+++ src/app/main-components/my-account/admin-data/admin-info-form/admin-info-form.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { Enterprise } from 'src/app/shared/models/enterprise.model';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { User } from 'src/app/shared/models/user.model';
+import { AlertsService } from 'src/app/shared/services/alerts.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import { EnterpriseService } from 'src/app/shared/services/enterprise.service';
 import { IconService } from 'src/app/shared/services/icon.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-admin-info-form',
@@ -15,12 +16,20 @@ export class AdminInfoFormComponent {
   constructor(
     private authService: AuthService,
     public icon:IconService,
-    private enterpriseService: EnterpriseService,
+    private alertService: AlertsService,
+    private userService: UserService,
 
   ) {}
 
+  @Output() onAdminInfoChange: EventEmitter<any> = new EventEmitter<any>()
+
+
   user: User
-  enterprise: Enterprise
+  adminUser: User
+
+  isEditing = false
+
+  form: FormGroup
 
   async ngOnInit(){
     this.authService.user$.subscribe(user=> {
@@ -28,10 +37,73 @@ export class AdminInfoFormComponent {
       
     })
 
-    await this.enterpriseService.whenEnterpriseLoaded()
-    this.enterprise = this.enterpriseService.getEnterprise()
-    
+    this.userService.getUsersObservable().subscribe(users => {
+      if(users.length > 0) {
+        const adminUsers = users.filter(x => x.role === "admin")
+        this.adminUser = adminUsers.length > 0? adminUsers[0]: null
+        console.log("this.adminUser")
+        console.log(this.adminUser)
+        this.initForm()
+      }
+    })
 
+  }
+
+  initForm() {
+    let firstName = "Carlos"
+    let email = "Email desconocido"
+    let job = "Cargo desconocido"
+    let secondName = "Martínez Encinas"
+    let phoneNumber = "Teléfono desconocido"
+    let country = "País desconocido"
+    let city = "Ciudad desconocida"
+    let zipCode = "Código postal desconocido"
+
+    // Aqui podemos definir firstName y secondName a partir del name del adminUser
+
+    //
+    
+    this.form =  new FormGroup({
+      "firstName": new FormControl(firstName),
+      "email": new FormControl(email),
+      "job": new FormControl(job),
+      "secondName": new FormControl(secondName),
+      "phoneNumber": new FormControl(phoneNumber),
+      "country": new FormControl(country),
+      "city": new FormControl(city),
+      "zipCode": new FormControl(zipCode),
+    })
+
+    if (this.adminUser) {
+      this.form.patchValue(this.adminUser)
+    }
+
+  }
+  
+  onClick() {
+    if (this.isEditing) {
+      this.onSubmit();
+    }
+    this.isEditing = !this.isEditing;
+  }
+
+  async onSubmit(){
+    const controls = this.form.controls
+    if (this.form.status === "VALID") {
+      this.onAdminInfoChange.emit(this.form.value)
+    }
+    else {
+      Object.keys(controls).forEach(prop => {
+        if (!controls[prop].valid && !controls[prop].disabled ) {
+          if (controls[prop].touched) {
+            console.log(`El valor de "${prop}" es invalido`)
+          }
+          else {
+            console.log(`Debes llenar el campo "${prop}"`)
+          }
+        }
+      });
+    }
   }
 
 }
