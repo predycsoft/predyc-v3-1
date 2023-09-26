6d2debd78df0acfd7bc1b99567e13262e37980d0
Diego Negrette <diegonegrette42@gmail.com>
2023-09-26 12:13:32 -0400

    finishing init script

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -54,7 +54,7 @@ export class UserService {
       );
       // const userCredential = await this.afAuth.createUserWithEmailAndPassword(email, password);
       // const user = userCredential.user;
-      await this.afs.collection(User.collection).doc(uid).set({...newUser.toJson(), id: uid});
+      await this.afs.collection(User.collection).doc(uid).set({...newUser.toJson(), uid: uid});
       newUser.uid = uid
       this.alertService.succesAlert('Has agregado un nuevo usuario exitosamente.')
     } catch (error) {
