0bfe66fac7353fb5a66cb52d9b7bb022a0ce72a2
Diego Negrette <diegonegrette42@gmail.com>
2023-10-05 16:10:02 -0400

    Adding style to license component

--- src/app/main-components/settings/license/plan-card/plan-card.component.ts
+++ src/app/main-components/settings/license/plan-card/plan-card.component.ts
@@ -8,6 +8,7 @@ import { CouponService } from 'src/app/shared/services/coupon.service';
 import { EnterpriseService } from 'src/app/shared/services/enterprise.service';
 import { IconService } from 'src/app/shared/services/icon.service';
 import { LicenseService } from 'src/app/shared/services/license.service';
+import { LoaderService } from 'src/app/shared/services/loader.service';
 import { PriceService } from 'src/app/shared/services/price.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 
@@ -24,6 +25,7 @@ export class PlanCardComponent {
     private couponService: CouponService,
     private enterpriseService: EnterpriseService,
     private afs: AngularFirestore,
+    public loaderService: LoaderService,
     public icon: IconService,
   ) {}
 
@@ -35,6 +37,7 @@ export class PlanCardComponent {
   loaded: boolean = false
 
   async ngOnInit() {
+    this.loaderService.setLoading(true)
     this.enterpriseService.enterpriseLoaded$.subscribe(isLoaded => {
       if (isLoaded) {
         const enterpriseRef = this.enterpriseService.getEnterpriseRef();
@@ -47,6 +50,7 @@ export class PlanCardComponent {
                           ? await this.couponService.getCouponByRef(this.price.coupon as DocumentReference)
                           : null
             this.loaded = true
+            this.loaderService.setLoading(false)
           }
         })
       }
