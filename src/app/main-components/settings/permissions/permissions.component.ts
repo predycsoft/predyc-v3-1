8a8688c0b613ccf32eee3db318035f4d0067aef7
Diego Negrette <diegonegrette42@gmail.com>
2023-10-13 15:54:40 -0400

    Improving permissions view

--- src/app/main-components/settings/permissions/permissions.component.ts
+++ src/app/main-components/settings/permissions/permissions.component.ts
@@ -15,40 +15,52 @@ export class Model {
   generation: "optimum" |"confirm" |"default" | null
   attempts: number | null
 
-  public static OPTION_FREE: string = 'free'
-  public static OPTION_STRICT: string = 'strict'
-  public static OPTION_REQUEST: string = 'request'
-  public static OPTION_OPTIMUM: string = 'optimum'
-  public static OPTION_CONFIRM: string = 'confirm'
-  public static OPTION_DEFAULT: string = 'default'
+  public static OPTION_BASIC: 'basic' = 'basic'
+  public static OPTION_AVERAGE: 'average' = 'average'
+  public static OPTION_STANDARD: 'standard' = 'standard'
+  public static OPTION_FREE: 'free' = 'free'
+  public static OPTION_STRICT: 'strict' = 'strict'
+  public static OPTION_REQUEST: 'request' = 'request'
+  public static OPTION_OPTIMUM: 'optimum' = 'optimum'
+  public static OPTION_CONFIRM: 'confirm' = 'confirm'
+  public static OPTION_DEFAULT: 'default' = 'default'
+
+  public static HOURS_OPTIONS = [Model.OPTION_BASIC, Model.OPTION_AVERAGE, Model.OPTION_STANDARD]
+  public static LIBERTY_OPTIONS = [Model.OPTION_FREE, Model.OPTION_STRICT, Model.OPTION_REQUEST]
+  public static GENERATION_OPTIONS = [Model.OPTION_OPTIMUM, Model.OPTION_CONFIRM, Model.OPTION_DEFAULT]
+  public static ATTEMPTS_OPTIONS = [Model.OPTION_STRICT, Model.OPTION_BASIC, Model.OPTION_AVERAGE]
 
   public static LIBERTY_OPTIONS_TO_DISPLAY = ["Libre", "Estricto", "Solicitudes"]
   public static GENERATION_OPTIONS_TO_DISPLAY = ["Óptimo", "Confirmar", "Por defecto"]
-  public static LIBERTY_OPTIONS = [this.OPTION_FREE, this.OPTION_STRICT, this.OPTION_REQUEST]
-  public static GENERATION_OPTIONS = [this.OPTION_OPTIMUM, this.OPTION_CONFIRM, this.OPTION_DEFAULT]
 
+  private static OPTIONS_MAP = {
+    [Model.OPTION_FREE]: "Libre",
+    [Model.OPTION_STRICT]: "Estricto",
+    [Model.OPTION_BASIC]: "Básico",
+    [Model.OPTION_AVERAGE]: "Promedio",
+    [Model.OPTION_STANDARD]: "Estándar",
+    [Model.OPTION_REQUEST]: "Solicitudes",
+    [Model.OPTION_OPTIMUM]: "Óptimo",
+    [Model.OPTION_CONFIRM]: "Confirmar",
+    [Model.OPTION_DEFAULT]: "Promedio",
+  };
+  
+  public static mapStringOptions(option: string): string {
+    return this.OPTIONS_MAP[option] || option;
+  }
 
-  public static mapOptions(option: 
-    typeof Model.OPTION_FREE | 
-    typeof Model.OPTION_STRICT | 
-    typeof Model.OPTION_REQUEST | 
-    typeof Model.OPTION_OPTIMUM | 
-    typeof Model.OPTION_CONFIRM | 
-    typeof Model.OPTION_DEFAULT
-  ): string {
-    let index: number
-    if (option === Model.OPTION_FREE || option === Model.OPTION_STRICT || option === Model.OPTION_REQUEST ) {
-      index = this.LIBERTY_OPTIONS.indexOf(option);
-      if (index > -1) {
-        return this.LIBERTY_OPTIONS_TO_DISPLAY[index];
-      }
-    } else {
-      index = this.GENERATION_OPTIONS.indexOf(option);
-      if (index > -1) {
-        return this.GENERATION_OPTIONS_TO_DISPLAY[index];
-      }
-    }
-    return option;  // retorna el valor original si no hay equivalencia
+  public static mapNumberOptions(option: number, field: string): string {
+    const hourBasicCase = [1, 2, 3]
+    const hourStandardCase = [6, 7, 8]
+
+    const attemptStrictCase = 1
+    const attemptBasicCase = 2
+
+    const hourValue = hourBasicCase.includes(option) ? Model.OPTION_BASIC : hourStandardCase.includes(option) ? Model.OPTION_STANDARD : Model.OPTION_AVERAGE
+    const attemptValue = option === attemptStrictCase ? Model.OPTION_STRICT : option === attemptBasicCase ? Model.OPTION_BASIC : Model.OPTION_AVERAGE
+    
+    if (field === "hours") return this.mapStringOptions(hourValue)
+    return this.mapStringOptions(attemptValue) 
   }
 }
 
@@ -71,14 +83,14 @@ export class PermissionsComponent {
 
   // From firebase
   values: Model = {
-    hours : 9,
-    liberty : "request",
-    generation : "confirm",
+    hours : null,
+    liberty :"request",
+    generation : null,
     attempts : 2,
   }
 
   defaultFormValues: Model = {
-    hours : 6,
+    hours : 4,
     liberty : "strict",
     generation : "optimum",
     attempts : 3,
@@ -86,18 +98,20 @@ export class PermissionsComponent {
 
   ngOnInit() {
     this.form =  new FormGroup({
-      "hours": new FormControl(null),
-      "liberty": new FormControl(""),
-      "generation": new FormControl(""),
-      "attempts": new FormControl(null),
+      "hours": new FormControl(this.defaultFormValues.hours),
+      "liberty": new FormControl(this.defaultFormValues.liberty),
+      "generation": new FormControl(this.defaultFormValues.generation),
+      "attempts": new FormControl(this.defaultFormValues.attempts),
     })
 
     if (this.values){
-      this.form.patchValue(this.values)
-      
-    }
-    else {
-      this.form.patchValue(this.defaultFormValues)
+      const nonNullValues = Object.keys(this.values).reduce((acc, key) => {
+          if (this.values[key] !== null) {
+              acc[key] = this.values[key];
+          }
+          return acc;
+      }, {});
+      this.form.patchValue(nonNullValues);
     }
 
     this.form.valueChanges.subscribe(value => {
@@ -108,7 +122,7 @@ export class PermissionsComponent {
     console.log("this.form.value", this.form.value)
   }
 
-  mapOptions(option: 
+  mapStringOptions(option: 
     typeof Model.OPTION_FREE | 
     typeof Model.OPTION_STRICT | 
     typeof Model.OPTION_REQUEST | 
@@ -116,7 +130,45 @@ export class PermissionsComponent {
     typeof Model.OPTION_CONFIRM | 
     typeof Model.OPTION_DEFAULT
   ): string {
-    return Model.mapOptions(option)
+    return Model.mapStringOptions(option)
+  }
+
+  mapNumberOptions(option: number, field: "hours" | "attempts"): string {
+    return Model.mapNumberOptions(option, field)
+  }
+
+  getBlockClass(value: string | number, position: number, field: string): string {
+    const styles = {
+      hours: {
+        1: ['red', 'red-op', 'red-op2'],
+        2: ['red', 'red-op', 'red-op2'],
+        3: ['red', 'red-op', 'red-op2'],
+        4: ['yellow', 'yellow', 'yellow-op'],
+        5: ['yellow', 'yellow', 'yellow-op'],
+        6: ['green', 'green', 'green'],
+        7: ['green', 'green', 'green'],
+        8: ['green', 'green', 'green'],
+      },
+      liberty: {
+        free: ['green', 'green', 'green'],
+        strict: ['yellow', 'yellow', 'yellow-op'],
+        request: ['red', 'red-op', 'red-op2']
+      },
+      generation: {
+        optimum: ['green', 'green', 'green'],
+        confirm: ['yellow', 'yellow', 'yellow-op'],
+        default: ['red', 'red-op', 'red-op2']
+      },
+      attempts: {
+        1: ['red', 'red-op', 'red-op2'],
+        2: ['yellow', 'yellow', 'yellow-op'],
+        3: ['green', 'green', 'green'],
+        4: ['green', 'green', 'green'],
+        5: ['green', 'green', 'green'],
+      }
+    };
+
+    return styles[field] && styles[field][value] ? styles[field][value][position - 1] : '';
   }
 
 }
