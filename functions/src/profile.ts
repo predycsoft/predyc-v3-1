cdfa9c95fbd95bd35a2c9eed1bf27eb029df3e08
Diego Negrette <diegonegrette42@gmail.com>
2023-10-26 11:12:34 -0400

    onCreate profile cloud function bug fixed

--- functions/src/profile.ts
+++ functions/src/profile.ts
@@ -6,28 +6,26 @@ const db = admin.firestore();
 export const onProfileAdded = functions.firestore.document('profile/{doc}').onCreate(async (snap, _) => {
     const profile = snap.data();
     
-    if (!profile.enterpriseRef || !Array.isArray(profile.enterpriseRef)) {
-        console.log("El perfil no tiene un array de enterpriseRef")
+    if (!profile.enterpriseRef || !(profile.enterpriseRef instanceof admin.firestore.DocumentReference)) {
+        console.log("El perfil no tiene enterpriseRef como DocumentReference");
         return;
     }
 
-    for (const enterpriseRef of profile.enterpriseRef) {
-        const enterprise = (await enterpriseRef.get()).data();
-        
-        if (!enterprise) {
-            console.error('Enterprise data not found');
-            continue;
-        }
-        return db.collection('enterprise').doc(enterprise.id).update(
-            {
-            profilesNo: admin.firestore.FieldValue.increment(1)
-            }
-        ).then(() => {
-            console.log(
-                `Updated enterprise: ${enterprise.name} profiles quantity to: ${enterprise.profilesNo + 1}`
-            );
-        }).catch((error) => {
-            console.error(error);
-        });
+    const enterpriseRef = profile.enterpriseRef;
+    const enterprise = (await enterpriseRef.get()).data();
+
+    if (!enterprise || !enterprise.id) {
+        console.error('Enterprise data not found');
+        return;
     }
+
+    return db.collection('enterprise').doc(enterprise.id).update(
+        {
+            profilesNo: admin.firestore.FieldValue.increment(1)
+        }
+    ).then(() => {
+        console.log(`Updated enterprise: ${enterprise.name} profiles quantity to: ${enterprise.profilesNo + 1}`);
+    }).catch((error) => {
+        console.error(error);
+    });
 });
