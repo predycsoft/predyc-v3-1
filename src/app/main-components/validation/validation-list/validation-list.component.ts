5026f43dd138f9a76f0f8ebd85a5283248d2ed24
Diego Negrette <diegonegrette42@gmail.com>
2023-11-09 17:01:06 -0400

    sorting validation list by title

--- src/app/main-components/validation/validation-list/validation-list.component.ts
+++ src/app/main-components/validation/validation-list/validation-list.component.ts
@@ -2,11 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { IconService } from 'src/app/shared/services/icon.service';
 import { DataSource } from '@angular/cdk/collections';
 import { MatPaginator } from '@angular/material/paginator';
-import { BehaviorSubject, Observable, Subscription, catchError, combineLatest, map, merge, of } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription, catchError, combineLatest, map, merge, of } from 'rxjs';
 import { Activity } from 'src/app/shared/models/activity-classes.model';
 import { ActivityClassesService } from 'src/app/shared/services/activity-classes.service';
 import { EnterpriseService } from 'src/app/shared/services/enterprise.service';
 import { SearchInputService } from 'src/app/shared/services/search-input.service';
+import { MatSort } from '@angular/material/sort';
+import { orderByValueAndDirection } from 'src/app/shared/utils';
 
 @Component({
   selector: 'app-validation-list',
@@ -27,6 +29,8 @@ export class ValidationListComponent {
   enablePagination: boolean = true
   pageSize: number = 4
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
 
   searchSubscription: Subscription
 
@@ -44,7 +48,8 @@ export class ValidationListComponent {
           this.activityService,
           this.paginator,
           this.pageSize,
-          this.enablePagination
+          this.enablePagination,
+          this.sort
         );
       }
     })
@@ -66,24 +71,28 @@ class ActivityDataSource extends DataSource<Activity> {
 
   private filterSubject = new BehaviorSubject<string>('');
 
+  private sortSubject = new Subject<void>();
+
 
   constructor(
     public activityService: ActivityClassesService,
     private paginator: MatPaginator,
     private pageSize: number,
-    private enablePagination: boolean
+    private enablePagination: boolean,
+    private sort: MatSort,
   ) {
     super();
 
     if (this.enablePagination) {
       this.paginator.pageSize = this.pageSize
     }
+    this.sort.sortChange.subscribe(() => this.sortSubject.next());
 
   }
 
   
   connect(): Observable<Activity[]> {
-    return merge(this.activityService.activities$, this.filterSubject, this.paginator.page).pipe(
+    return merge(this.activityService.activities$, this.filterSubject, this.paginator.page, this.sortSubject).pipe(
       map(() => {
         const activities = this.activityService.getActivitesSubjectValue()
         if (this.enablePagination) {
@@ -92,14 +101,35 @@ class ActivityDataSource extends DataSource<Activity> {
 
         this.activities = [...activities]
 
-        let filteredUsers = this.activities.filter(activity => {
+        // Search bar
+        let filteredActivities = this.activities.filter(activity => {
           const searchStr = (activity.title as string).toLowerCase();
           return searchStr.indexOf(this.filterSubject.value.toLowerCase()) !== -1;
         });
 
+        // Sorting
+        if (this.sort.active && this.sort.direction !== '') {
+          filteredActivities = filteredActivities.sort((a, b) => {
+            const isAsc = this.sort.direction === 'asc';
+            // console.log('this.sort.active')
+            // console.log(this.sort.active)
+            switch (this.sort.active) {
+              case 'title': return orderByValueAndDirection(a.title as string, b.title as string, isAsc);
+              // case 'status': return this.utilsService.compare(a.status as string, b.status as string, isAsc);
+              // case 'createdAt': return 0
+              // case 'updatedAt': return 0
+              // case 'grade': return 0
+              // case 'assigned': return 0
+              // case 'performance': return 0
+              // Add more fields to sort by as needed.
+              default: return 0;
+            }
+          });
+        }
+
         // Pagination
         const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-        return filteredUsers.splice(startIndex, this.paginator.pageSize);
+        return filteredActivities.splice(startIndex, this.paginator.pageSize);
       }),
       catchError(error => {
         console.error('Error occurred:', error);
