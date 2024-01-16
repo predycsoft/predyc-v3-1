37f47e3b14a4dfc8b51932860fbd83ec9fa3b465
Diego Negrette <diegonegrette42@gmail.com>
2024-01-16 10:55:24 -0400

    Lo siento

--- src/app/shared/components/users/license-student-list/license-student-list.component.ts
+++ src/app/shared/components/users/license-student-list/license-student-list.component.ts
@@ -3,8 +3,8 @@ import { Component, EventEmitter, Input, Output, SimpleChanges, ViewChild } from
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { Subscription } from 'rxjs';
-import { Subscription } from 'src/app/shared/models/subscription.model'
+import { Subscription } from 'rxjs';
+import { Subscription as SubscriptionClass } from 'src/app/shared/models/subscription.model'
 import { Profile } from 'src/app/shared/models/profile.model';
 import { IconService } from 'src/app/shared/services/icon.service';
 import { ProfileService } from 'src/app/shared/services/profile.service';
@@ -39,9 +39,9 @@ export class LicenseStudentListComponent {
   @Input() hasLicenseChanged: any;
 
 
-  queryParamsSubscription
-  profilesSubscription
-  userServiceSubscription
+  queryParamsSubscription: Subscription
+  profilesSubscription: Subscription
+  userServiceSubscription: Subscription
   pageSize: number = 7
   totalLength: number
   profiles: Profile[] = []
@@ -52,7 +52,7 @@ export class LicenseStudentListComponent {
     this.allowMultiSelect, this.initialSelection
   );
 
-  private lastStatusFilter: string = 'active'
+  private lastStatusFilter: string = SubscriptionClass.STATUS_ACTIVE
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -70,7 +70,7 @@ export class LicenseStudentListComponent {
         this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe(params => {
           const page = Number(params['page']) || 1;
           const searchTerm = params['search'] || '';
-          const statusFilter = params['status'] || 'active';
+          const statusFilter = params['status'] || SubscriptionClass.STATUS_ACTIVE;
           // clear checkboxes selection if status filter changed
           if (this.lastStatusFilter !== statusFilter) {
             this.selection.clear(); 
@@ -100,8 +100,8 @@ export class LicenseStudentListComponent {
     }
     this.userServiceSubscription = this.userService.getUsers$(searchTerm, null, statusFilter).subscribe(
       response => {
-        if (statusFilter != 'active') {
-          response = response.filter(item => item.status !== 'active')
+        if (statusFilter != SubscriptionClass.STATUS_ACTIVE) {
+          response = response.filter(item => item.status !== SubscriptionClass.STATUS_ACTIVE)
         }
         const users: LicenseListUser[] = response.map(item => {
           // Seting profile
@@ -118,7 +118,7 @@ export class LicenseStudentListComponent {
             displayName: item.displayName,
             profile: profileName,
             status: item.status,
-            statusDisplayText: Subscription.statusToDisplayValueDict[item.status],
+            statusDisplayText: SubscriptionClass.statusToDisplayValueDict[item.status],
             uid: item.uid
           }
           return user
