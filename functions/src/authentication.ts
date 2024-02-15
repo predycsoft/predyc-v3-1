233b3c14727e8eb53e986c7ce7c8c6dc4840f51e
Diego Negrette <diegonegrette42@gmail.com>
2024-02-15 17:26:41 +0100

    Added create demo component

--- functions/src/authentication.ts
+++ functions/src/authentication.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { _sendMail } from './email'
 
-// const db = admin.firestore();
+const db = admin.firestore();
 
 export const createUserWithEmailAndPassword = functions.https.onCall(
     async (data, _) => {
@@ -31,4 +31,70 @@ export const createUserWithEmailAndPassword = functions.https.onCall(
             throw new functions.https.HttpsError('unknown', error.message);
         } 
     }
-  );
+);
+
+const batchSize = 10
+
+export const emptyDatabase = functions.https.onCall(
+    async (data, _) => {
+        try {
+            const users = await db.collection('user').listDocuments()
+            const uids = users.map(user => user.id)
+            const collections = [
+                // 'activity',
+                'category',
+                'class',
+                'coupon',
+                'classByStudent',
+                'course',
+                'coursesByStudent',
+                'department',
+                'general',
+                'instructors',
+                'license',
+                'notification',
+                'price',
+                'product',
+                'profile',
+                'skill',
+                'user',
+                'enterprise',
+                'userProfile'
+            ]
+            // for (let collection of collections) {
+            //     await deleteCollection(collection)
+            // }
+            // await admin.auth().deleteUsers(uids)
+        } catch (error: any) {
+            console.log(error)
+            throw new functions.https.HttpsError('unknown', error.message);
+        } 
+    }
+);
+
+// async function deleteCollection(
+//     collectionPath: string,
+// ): Promise<number> {
+//     const collectionRef = db.collection(collectionPath);
+//     const query = collectionRef.orderBy('__name__').limit(batchSize);
+//     const snapshot = await query.get();
+
+//     // When there are no documents left, we are done
+//     if (snapshot.size === 0) {
+//         return 0;
+//     }
+
+//     const batch = db.batch();
+//     console.log(snapshot.docs)
+//     snapshot.docs.forEach(async doc => {
+//         batch.delete(doc.ref);
+//         // const subcollections = await doc.ref.listCollections();
+//         // subcollections.forEach(async subcollection => {
+//         //     await deleteCollection(subcollection.path, batchSize);
+//         // });
+//     });
+
+//     await batch.commit();
+
+//     return 1;
+// }
