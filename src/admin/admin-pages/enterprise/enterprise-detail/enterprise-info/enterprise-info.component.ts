a8ff0e71104a1abf64174edd6df617f9bc4a7799
diego negrette <diegonegrette42@gmail.com>
2024-03-01 16:26:19 -0400

    enterprise info, students and payments components created

---
+++ src/admin/admin-pages/enterprise/enterprise-detail/enterprise-info/enterprise-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enterprise-info',
+  templateUrl: './enterprise-info.component.html',
+  styleUrls: ['./enterprise-info.component.css']
+})
+export class EnterpriseInfoComponent {
+
+}
---
+++ src/admin/admin-pages/enterprise/enterprise-detail/enterprise-students/enterprise-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enterprise-students',
+  templateUrl: './enterprise-students.component.html',
+  styleUrls: ['./enterprise-students.component.css']
+})
+export class EnterpriseStudentsComponent {
+
+}
