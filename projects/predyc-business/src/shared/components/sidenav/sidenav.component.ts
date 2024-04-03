539311a9f70226f2ebad4ac0985cd0d463649c85
Diego Negrette <diegonegrette42@gmail.com>
2024-04-04 00:41:16 +0200

    Comment sidenav

--- projects/predyc-business/src/shared/components/sidenav/sidenav.component.ts
+++ projects/predyc-business/src/shared/components/sidenav/sidenav.component.ts
@@ -6,112 +6,112 @@ import { User } from "projects/shared/models/user.model";
 import { Observable } from "rxjs";
 
 interface Page {
-	link: string;
-	name: string;
-	icon: string;
+  link: string;
+  name: string;
+  icon: string;
 }
 
 @Component({
-	selector: "app-sidenav",
-	templateUrl: "./sidenav.component.html",
-	styleUrls: ["./sidenav.component.css"],
-	animations: [onSideNavChange, animateText],
+  selector: "app-sidenav",
+  templateUrl: "./sidenav.component.html",
+  styleUrls: ["./sidenav.component.css"],
+  animations: [onSideNavChange, animateText],
 })
 export class SideNavComponent {
-	user: User;
-	user$: Observable<User> = this.authService.user$;
+  user: User;
+  user$: Observable<User> = this.authService.user$;
 
-	public linkText: boolean = false;
+  public linkText: boolean = false;
 
-	public pages: Page[] = [];
+  public pages: Page[] = [];
 
-	public businessPages: Page[] = [
-		{
-			name: "Dashboard",
-			link: "",
-			icon: "../../assets/iconsUI/dashboard-1.svg",
-		},
-		{
-			name: "Estudiantes",
-			link: "management/students",
-			icon: "../../assets/iconsUI/management-1.svg",
-		},
-		{
-			name: "Cursos",
-			link: "management/courses",
-			icon: "../../assets/iconsUI/courses-1.svg",
-		},
-		{
-			name: "Licencias",
-			link: "settings",
-			icon: "../../assets/iconsUI/settings-1.svg",
-		},
-	];
+  public businessPages: Page[] = [
+    {
+      name: "Dashboard",
+      link: "",
+      icon: "../../assets/iconsUI/dashboard-1.svg",
+    },
+    {
+      name: "Estudiantes",
+      link: "management/students",
+      icon: "../../assets/iconsUI/management-1.svg",
+    },
+    {
+      name: "Cursos",
+      link: "management/courses",
+      icon: "../../assets/iconsUI/courses-1.svg",
+    },
+    {
+      name: "Licencias",
+      link: "settings",
+      icon: "../../assets/iconsUI/settings-1.svg",
+    },
+  ];
 
-	public adminPages: Page[] = [
-		{
-			name: "Principal",
-			link: "/admin",
-			icon: "../../assets/iconsUI/home.svg",
-		},
-		{
-			name: "Estudiantes",
-			link: "/admin/students",
-			icon: "../../assets/iconsUI/management-1.svg",
-		},
-		{
-			name: "Empresas",
-			link: "/admin/enterprises",
-			icon: "../../assets/iconsUI/enterprise.svg",
-		},
-		{
-			name: "Crear demo",
-			link: "/admin/create-demo",
-			icon: "../../assets/iconsUI/demo.svg",
-		},
+  public adminPages: Page[] = [
+    {
+      name: "Principal",
+      link: "/admin",
+      icon: "../../assets/iconsUI/home.svg",
+    },
+    {
+      name: "Estudiantes",
+      link: "/admin/students",
+      icon: "../../assets/iconsUI/management-1.svg",
+    },
+    {
+      name: "Empresas",
+      link: "/admin/enterprises",
+      icon: "../../assets/iconsUI/enterprise.svg",
+    },
+    // {
+    // 	name: "Crear demo",
+    // 	link: "/admin/create-demo",
+    // 	icon: "../../assets/iconsUI/demo.svg",
+    // },
 
-		{
-			name: "Productos",
-			link: "/admin/products",
-			icon: "../../assets/iconsUI/bookmark.svg",
-		},
-		{
-			name: "L&S",
-			link: "/admin/licenses-and-subscriptions",
-			icon: "../../assets/iconsUI/Vector.svg",
-		},
-		{
-			name: "Regalias",
-			link: "/admin/royalties",
-			icon: "../../assets/iconsUI/suitcase.svg",
-		},
-		{
-			name: "Ventas",
-			link: "/admin/sales",
-			icon: "../../assets/iconsUI/catalog.svg",
-		},
-		{
-			name: "Cursos",
-			link: "/admin/courses",
-			icon: "../../assets/iconsUI/courses-1.svg",
-		},
-		{
-			name: "Freebies",
-			link: "/admin/freebies",
-			icon: "../../assets/iconsUI/puzzle.svg",
-		},
-	];
+    {
+      name: "Productos",
+      link: "/admin/products",
+      icon: "../../assets/iconsUI/bookmark.svg",
+    },
+    // {
+    // 	name: "L&S",
+    // 	link: "/admin/licenses-and-subscriptions",
+    // 	icon: "../../assets/iconsUI/Vector.svg",
+    // },
+    // {
+    // 	name: "Regalias",
+    // 	link: "/admin/royalties",
+    // 	icon: "../../assets/iconsUI/suitcase.svg",
+    // },
+    {
+      name: "Ventas",
+      link: "/admin/sales",
+      icon: "../../assets/iconsUI/catalog.svg",
+    },
+    {
+      name: "Cursos",
+      link: "/admin/courses",
+      icon: "../../assets/iconsUI/courses-1.svg",
+    },
+    {
+      name: "Freebies",
+      link: "/admin/freebies",
+      icon: "../../assets/iconsUI/puzzle.svg",
+    },
+  ];
 
-	constructor(public icon: IconService, private authService: AuthService) {}
+  constructor(public icon: IconService, private authService: AuthService) {}
 
-	@Input() menuExpanded = false;
-	@Input() currentUrl: string;
+  @Input() menuExpanded = false;
+  @Input() currentUrl: string;
 
-	ngOnChanges(changes: SimpleChanges) {
-		if (changes.currentUrl) {
-			this.pages = this.currentUrl.startsWith("/admin")
-				? this.adminPages
-				: this.businessPages;
-		}
-	}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.currentUrl) {
+      this.pages = this.currentUrl.startsWith("/admin")
+        ? this.adminPages
+        : this.businessPages;
+    }
+  }
 }
