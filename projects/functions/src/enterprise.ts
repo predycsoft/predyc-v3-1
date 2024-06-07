b794b8b50c49434c079201d5c008072a0e517e43
Arturo Romero <arturor1990@gmail.com>
2024-06-07 11:35:06 -0600

    ajuste en de cloud funtion estadisticas

---
+++ projects/functions/src/enterprise.ts
@@ -0,0 +1,41 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { Enterprise } from 'shared';
+
+const db = admin.firestore();
+
+// export const onProfileAdded = functions.firestore.document('profile/{doc}').onCreate(async (snap, _) => {
+//     const profile = snap.data();
+    
+//     if (!profile.enterpriseRef || !(profile.enterpriseRef instanceof admin.firestore.DocumentReference)) {
+//         console.log("El perfil no tiene enterpriseRef como DocumentReference");
+//         return;
+//     }
+
+//     const enterpriseRef = profile.enterpriseRef;
+//     const enterprise = (await enterpriseRef.get()).data();
+
+//     if (!enterprise || !enterprise.id) {
+//         console.error('Enterprise data not found');
+//         return;
+//     }
+
+//     return db.collection(Enterprise.collection).doc(enterprise.id).update(
+//         {
+//             profilesNo: admin.firestore.FieldValue.increment(1)
+//         }
+//     ).then(() => {
+//         console.log(`Updated enterprise: ${enterprise.name} profiles quantity to: ${enterprise.profilesNo + 1}`);
+//     }).catch((error) => {
+//         console.error(error);
+//     });
+// });
+
+export const updateDataEnterpriseUsage = functions.https.onCall(async (data, _) => {
+    try {
+
+    } catch (error: any) {
+      console.log(error);
+      throw new functions.https.HttpsError("unknown", error.message);
+    }
+  });
