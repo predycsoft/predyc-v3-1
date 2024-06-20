80e4d0e8f849b7e9449f698c045c6513d1265997
Arturo Romero <arturor1990@gmail.com>
2024-06-20 17:34:05 -0600

    ajustes ver videos en preguntas

--- projects/predyc-business/src/shared/components/questions/dialog-questions-detail/dialog-questions-detail.component.ts
+++ projects/predyc-business/src/shared/components/questions/dialog-questions-detail/dialog-questions-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Question } from 'projects/shared/models/question.model';
 import { IconService } from '../../../services/icon.service';
 import { CourseService } from '../../../services/course.service';
@@ -9,6 +9,7 @@ import { UserService } from '../../../services/user.service';
 import { QuestionService } from '../../../services/question.service';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
 import { capitalizeFirstLetter, firestoreTimestampToNumberTimestamp } from 'projects/shared/utils';
+import { VimeoComponent } from '../../vimeo/vimeo.component';
 
 interface CourseQuestionsData { // it has to be he same as the one declared in questions-list component
   courseQuestions: Question[]
@@ -44,7 +45,8 @@ export class DialogQuestionsDetailComponent {
     private courseService: CourseService, 
     private moduleService: ModuleService,
     private userService: UserService,
-    private questionService: QuestionService
+    private questionService: QuestionService,
+    private modalService: NgbModal,
   ) { }
 
   @Input() courseQuestionsData: CourseQuestionsData;
@@ -77,18 +79,21 @@ export class DialogQuestionsDetailComponent {
           const allClassIds = modules.flatMap(module => module.clasesRef.map(ref => ref.id));
           return this.courseService.getClassesByIds$(allClassIds).pipe(
             map(courseClasses => {
-              const classMap = new Map(courseClasses.map(courseClass => [courseClass.id, courseClass.titulo]));
-              return classMap;
+              return courseClasses;
             })
           );
         })
       ).subscribe(async classMap => {
         const preguntasPromises = this.courseQuestionsData.courseQuestions.map(async question => {
           const userData = await this.userService.getUserByUid(question.userRef.id)
+          const claseData = classMap.find(x=>x.id == question.claseRef.id)
           return {
             data: question,
             respondiendo: false,
-            claseTitulo: classMap.get(question.claseRef.id) || 'N/A',
+            claseTitulo: claseData?.titulo,
+            claseType:claseData.tipo,
+            vimeoId1:claseData.vimeoId1,
+            vimeoId2:claseData.vimeoId2,
             cursoTitulo: this.courseQuestionsData.courseTitle,
             instructorNombre: this.courseQuestionsData.instructorName,
             instructorFoto: this.courseQuestionsData.instructorPhoto,
@@ -153,6 +158,19 @@ export class DialogQuestionsDetailComponent {
 
   closeDialog() {
     this.activeModal.dismiss('Cross click');
+  }
+
+  verVideoVimeo(clase): any {
+
+    clase.titulo = clase.cursoTitulo + ' - ' + clase.claseTitulo
+
+    const modalRef = this.modalService.open(VimeoComponent, {
+      animation: true,
+      centered: true,
+      size: "lg",
+    });
+    modalRef.componentInstance.clase = clase;
+    return modalRef;
   }
 
 }
