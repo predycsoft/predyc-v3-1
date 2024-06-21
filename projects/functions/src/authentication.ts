9842760f46f52fe90eba6bf09abc3e97179bf812
Arturo Romero <arturor1990@gmail.com>
2024-06-21 15:09:54 -0600

    ajustes instructores y cloud fucntions

--- projects/functions/src/authentication.ts
+++ projects/functions/src/authentication.ts
@@ -1,9 +1,54 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { _sendMail } from "./email";
-import { User, capitalizeFirstLetter, generateSixDigitRandomNumber } from "shared";
+import { _sendMail, _sendMailHTML } from "./email";
+import { User, capitalizeFirstLetter, generateSixDigitRandomNumber, titleCase } from "shared";
 
 const db = admin.firestore();
+
+
+
+const firma = `<p>Saludos cordiales,</p>
+<img src="https://predictiva21.com/wp-content/uploads/2024/06/LOGOPREDYC-BACKWHITE.webp" alt="Predyc" style="width: 150px; height: auto;">`;
+const styleMail = `
+<style>
+  table {
+    max-width: 100%;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  .high {
+    color: green;
+  }
+  .medium {
+    color: orange;
+  }
+  .low {
+    color: red;
+  }
+  .no-iniciado, .no-plan {
+    color: gray;
+  }
+  .month-row {
+    border: none;
+    padding-top: 20px;
+    font-weight: bold;
+  }
+  .month-name {
+    padding-top: 20px;
+    font-weight: bold;
+    border: none;
+    text-align: left;
+  }
+</style>`;
+
+
 
 export const createUserWithEmailAndPassword = functions.https.onCall(async (data, _) => {
   try {
@@ -105,29 +150,82 @@ export const emptyDatabase = functions.https.onCall(async (data, _) => {
   }
 });
 
-// async function deleteCollection(
-//     collectionPath: string,
-// ): Promise<number> {
-//     const collectionRef = db.collection(collectionPath);
-//     const query = collectionRef.orderBy('__name__').limit(batchSize);
-//     const snapshot = await query.get();
 
-//     // When there are no documents left, we are done
-//     if (snapshot.size === 0) {
-//         return 0;
-//     }
+export const createInstructorWithEmailAndPassword = functions.https.onCall(async (data, _) => {
+  try {
+    const password = `${generateSixDigitRandomNumber()}`;
+    const userRecord = await admin.auth().createUser({
+      email: data.email,
+      password: password,
+    });
 
-//     const batch = db.batch();
-//     console.log(snapshot.docs)
-//     snapshot.docs.forEach(async doc => {
-//         batch.delete(doc.ref);
-//         // const subcollections = await doc.ref.listCollections();
-//         // subcollections.forEach(async subcollection => {
-//         //     await deleteCollection(subcollection.path, batchSize);
-//         // });
-//     });
+    // Enlace de restablecimiento de contraseña
+    const link = await _generatePasswordResetLink(data.email);
 
-//     await batch.commit();
+    const sender = "capacitacion@predyc.com";
 
-//     return 1;
-// }
+    const arrayName = data.name.split(" ")
+    arrayName.map((word: string) => capitalizeFirstLetter(word))
+    const name = arrayName.join(" ")
+
+    const recipients = [data.email];
+    const subject = "Bienvenido a Predyc Instructores, conoce tu usuario y contraseña temporal";
+
+
+    let correo = `<p>Hola <strong>${name}</strong>,</p>
+    <p>Se han generado sus credenciales para la plataforma de instructores de <strong>PREDYC</strong>. 
+    En esta plataforma podrá gestionar las preguntas que le dejan los alumnos y también las regalías que generan los cursos.</p>
+    <p>Sus credenciales son las siguientes:</p>
+    <p>Usuario: ${data.email}</p>
+    <p>Contraseña: ${password}</p><br>
+    <p>Puede ingresar a la plataforma mediante el siguiente enlace: <a href="https://predyc-empresa.web.app/login?email=${data.email}">Iniciar sesión</a>.</p>
+    <p>Si lo desea, puede también cambiar su contraseña en el siguiente enlace: <a href="${link}">Cambiar contraseña</a>.</p>`;    
+    let htmlMailFinal = `<!DOCTYPE html><html><head></head><body>${correo}<br>${firma}</body></html>`;
+    const htmlContent = htmlMailFinal;
+    const cc = ["desarrollo@predyc.com", "liliana.giraldo@predyc.com", "capacitacion@predyc.com"];
+    // const mailObj = { sender, recipients, subject, text, cc };
+    const mailObj = { sender, recipients, subject, cc, htmlContent};
+    await _sendMailHTML(mailObj);
+
+    return { uid: userRecord.uid };
+  } catch (error: any) {
+    console.log(error);
+    throw new functions.https.HttpsError("unknown", error.message);
+  }
+});
+
+export const rememberInstructorWithEmailAndPassword = functions.https.onCall(async (data, _) => {
+  try {
+
+    // Enlace de restablecimiento de contraseña
+    const link = await _generatePasswordResetLink(data.email);
+
+    const sender = "capacitacion@predyc.com";
+
+    const arrayName = data.name.split(" ")
+    arrayName.map((word: string) => capitalizeFirstLetter(word))
+    const name = arrayName.join(" ")
+
+    const recipients = [data.email];
+    const subject = "Bienvenido a Predyc Instructores, conoce tu usuario";
+
+
+    let correo = `<p>Hola <strong>${name}</strong>,</p>
+    <p>Se ha habilitado su cuenta para la plataforma de instructores de <strong>PREDYC</strong>. 
+    En esta plataforma podrá gestionar las preguntas que le dejan los alumnos y también las regalías que generan los cursos.</p>
+    <p>Sus credenciales son las siguientes:</p>
+    <p>Usuario: ${data.email}</p>
+    <p>Puede ingresar a la plataforma mediante el siguiente enlace: <a href="https://predyc-empresa.web.app/login?email=${data.email}">Iniciar sesión</a>.</p>
+    <p>Si lo desea, puede también cambiar su contraseña en el siguiente enlace: <a href="${link}">Cambiar contraseña</a>.</p>`;    
+    let htmlMailFinal = `<!DOCTYPE html><html><head></head><body>${correo}<br>${firma}</body></html>`;
+    const htmlContent = htmlMailFinal;
+    const cc = ["desarrollo@predyc.com", "liliana.giraldo@predyc.com", "capacitacion@predyc.com"];
+    // const mailObj = { sender, recipients, subject, text, cc };
+    const mailObj = { sender, recipients, subject, cc, htmlContent};
+    await _sendMailHTML(mailObj);
+    return true ;
+  } catch (error: any) {
+    console.log(error);
+    throw new functions.https.HttpsError("unknown", error.message);
+  }
+});
