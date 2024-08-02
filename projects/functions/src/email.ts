678e33638bde923a8ab11a3116589b8af001e86d
Arturo Romero <arturor1990@gmail.com>
2024-08-01 20:00:12 -0600

    bug fix cursos

--- projects/functions/src/email.ts
+++ projects/functions/src/email.ts
@@ -6,7 +6,7 @@ import * as admin from "firebase-admin";
 const db = admin.firestore();
 
 export const _sendMailHTML = async (data: { sender: string; recipients: string[]; subject: string; cc: string[]; htmlContent: string; adjunto?: any }) => {
-  const APP_NAME = "Predyc";
+  let APP_NAME = "Predyc";
 
   let sender = process.env.EMAIL_USER_D;
   let password = process.env.EMAIL_PASSWORD_D;
@@ -17,6 +17,7 @@ export const _sendMailHTML = async (data: { sender: string; recipients: string[]
   } else if (['capacitacion@predictiva21.com'].includes(data.sender)) {
     sender = process.env.EMAIL_USER_P21_CAP;
     password = process.env.EMAIL_PASSWORD_P21_CAP;
+    APP_NAME = "Predictiva21";
   }
 
   console.log("_sendMailHTML data.htmlContent", data.htmlContent);
@@ -67,9 +68,7 @@ export const _sendMailHTML = async (data: { sender: string; recipients: string[]
 };
 
 export const _sendMail = async (data: { sender: string; recipients: string[]; subject: string; text: string; cc: string[]; htmlText?: string }) => {
-  const APP_NAME = "Predyc";
-
-  console.log("PRUEBA");
+  let APP_NAME = "Predyc";
 
   // let sender = process.env.EMAIL_USER_A
   // let password = process.env.EMAIL_PASSWORD_A
@@ -83,6 +82,7 @@ export const _sendMail = async (data: { sender: string; recipients: string[]; su
   } else if (['capacitacion@predictiva21.com'].includes(data.sender)) {
     sender = process.env.EMAIL_USER_P21_CAP;
     password = process.env.EMAIL_PASSWORD_P21_CAP;
+    APP_NAME = "Predictiva21";
   }
 
   // if (["contacto@predyc.com", "capacitacion@predyc.com"].includes(data.sender) ){
