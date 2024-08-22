7ffaf48b7ff76b73d1f824aabc264cd1469d41a6
Arturo Romero <arturor1990@gmail.com>
2024-08-22 12:56:24 -0600

    agregado correo de ventas

--- projects/functions/src/email.ts
+++ projects/functions/src/email.ts
@@ -19,6 +19,10 @@ export const _sendMailHTML = async (data: { sender: string; recipients: string[]
     password = process.env.EMAIL_PASSWORD_P21_CAP;
     APP_NAME = "Predictiva21";
   }
+  else if (['ventas@predyc.com'].includes(data.sender)) {
+    sender = process.env.EMAIL_VENTAS;
+    password = process.env.EMAIL_VENTAS;
+  }
 
   console.log("_sendMailHTML data.htmlContent", data.htmlContent);
 
@@ -83,6 +87,10 @@ export const _sendMail = async (data: { sender: string; recipients: string[]; su
     sender = process.env.EMAIL_USER_P21_CAP;
     password = process.env.EMAIL_PASSWORD_P21_CAP;
     APP_NAME = "Predictiva21";
+  }
+  else if (['ventas@predyc.com'].includes(data.sender)) {
+    sender = process.env.EMAIL_VENTAS;
+    password = process.env.EMAIL_VENTAS;
   }
 
   // if (["contacto@predyc.com", "capacitacion@predyc.com"].includes(data.sender) ){
