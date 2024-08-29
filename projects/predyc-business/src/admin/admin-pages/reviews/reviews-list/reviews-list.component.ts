92a5d9330dfac717f269c9a28f26bcc6e46cda39
Diego Negrette <diegonegrette42@gmail.com>
2024-08-29 14:15:01 +0200

    Showing reviews list

--- projects/predyc-business/src/admin/admin-pages/reviews/reviews-list/reviews-list.component.ts
+++ projects/predyc-business/src/admin/admin-pages/reviews/reviews-list/reviews-list.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { combineLatest, Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CourseService } from 'projects/predyc-business/src/shared/services/course.service';
+
+export interface CourseRatingData {
+  courseName: string;
+  courseId: string;
+  averageRating: number;
+  totalRatings: number;
+  totalCompleted: number;
+}
 
 @Component({
   selector: 'app-reviews-list',
@@ -6,5 +19,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews-list.component.css']
 })
 export class ReviewsListComponent {
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private courseService: CourseService,
+
+  ) {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  dataSource = new MatTableDataSource<CourseRatingData>();
+  displayedColumns: string[] = [
+    "courseName",
+    "averageRating",
+    "totalRatings",
+  ];
+
+  pageSize: number = 2
+  totalLength: number
+  
+  queryParamsSubscription: Subscription
+  reviewsSubscription: Subscription
+  courseServiceSubscription: Subscription
+
+  ngOnInit() {
+    this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe(params => {
+      const page = Number(params['page']) || 1;
+      // const searchTerm = params['search'] || '';
+      this.performSearch(page);
+    })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator.pageSize = this.pageSize;
+  }
+
+  performSearch(page: number) {
+
+    this.courseServiceSubscription = combineLatest([
+      this.courseService.getCoursesRatings$(),
+      this.courseService.getAllCourses$()  
+    ]).subscribe(([ratings, courses]) => {
+      const courseRatingsData = []
+      courses.forEach(course => {
+        // Filter ratings for the current course
+        const courseRatings = ratings.filter(rating => rating.courseRef.id === course.id);
+
+        // Calculate metrics
+        const totalRatings = courseRatings.length;
+        const averageRating = courseRatings.reduce((sum, rating) => sum + rating.valoracion.global, 0) / totalRatings || 0;
+        const totalCompleted = courseRatings.filter(rating => rating.valoracion.completado).length;
+
+        // Push the metrics to the courseRatings array
+        courseRatingsData.push({
+          courseName: course.titulo,
+          courseId: course.id,
+          averageRating: averageRating,
+          totalRatings: totalRatings,
+          totalCompleted: totalCompleted
+        });
+      });
+
+    courseRatingsData.sort((a, b) => b.averageRating - a.averageRating);
+
+    // console.log("courseRatingsData", this.courseRatingsData);
+
+    this.paginator.pageIndex = page - 1;
+    this.dataSource.data = courseRatingsData;
+    this.totalLength = courseRatingsData.length;
+
+    })
+  }
+  
+
+  onPageChange(page: number): void {
+    this.router.navigate([], {
+      queryParams: { page },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  openCourseReviewsModal(review: CourseRatingData) {
+
+  }
+
+  ngOnDestroy() {
+    if (this.courseServiceSubscription) this.courseServiceSubscription.unsubscribe();
+  }
+
+
 
 }
--- projects/predyc-business/src/admin/admin-pages/reviews/reviews.component.ts
+++ projects/predyc-business/src/admin/admin-pages/reviews/reviews.component.ts
@@ -1,15 +1,4 @@
 import { Component } from '@angular/core';
-import { CourseService } from 'projects/predyc-business/src/shared/services/course.service';
-import { combineLatest, Subscription } from 'rxjs';
-
-interface CourseRatingData {
-  courseName: string;
-  courseId: string;
-  averageRating: number;
-  totalRatings: number;
-  totalCompleted: number;
-}
-
 
 @Component({
   selector: 'app-reviews',
@@ -19,42 +8,12 @@ interface CourseRatingData {
 export class ReviewsComponent {
 
   constructor(
-    private courseService: CourseService,
   ) {}
 
-  courseServiceSubscription: Subscription
-  courseRatings: CourseRatingData[] = []
 
   ngOnInit() {
-    this.courseServiceSubscription = combineLatest([
-      this.courseService.getCoursesRatings$(),
-      this.courseService.getAllCourses$()  
-    ]).subscribe(([ratings, courses]) => {
-      console.log("ratings", ratings)
-      
-      courses.forEach(course => {
-        // Filter ratings for the current course
-        const courseRatings = ratings.filter(rating => rating.courseRef.id === course.id);
 
-        // Calculate metrics
-        const totalRatings = courseRatings.length;
-        const averageRating = courseRatings.reduce((sum, rating) => sum + rating.valoracion.global, 0) / totalRatings || 0;
-        const totalCompleted = courseRatings.filter(rating => rating.valoracion.completado).length;
-
-        // Push the metrics to the courseRatings array
-        this.courseRatings.push({
-            courseName: course.titulo,
-            courseId: course.id,
-            averageRating: averageRating,
-            totalRatings: totalRatings,
-            totalCompleted: totalCompleted
-        });
-      });
-
-    this.courseRatings.sort((a, b) => b.averageRating - a.averageRating);
-
-    console.log("courseRatings", this.courseRatings);
-
-    })
   }
+
+
 }
