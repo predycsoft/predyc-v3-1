c4e52e69efd44b139f1c7c3f7eb963b4e9041eaf
Diego Negrette <diegonegrette42@gmail.com>
2024-10-28 13:08:36 -0400

    student list subscriptions to promises

--- projects/predyc-business/src/shared/components/users/student-list/student-list.component.ts
+++ projects/predyc-business/src/shared/components/users/student-list/student-list.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core
 import { MatPaginator } from '@angular/material/paginator';
 import { IconService } from 'projects/predyc-business/src/shared/services/icon.service';
 import { UserService } from 'projects/predyc-business/src/shared/services/user.service';
-import { combineLatest, filter, forkJoin, map, merge, mergeMap, Observable, of, Subscription, switchMap } from 'rxjs';
+import { combineLatest, filter, firstValueFrom, forkJoin, map, merge, mergeMap, Observable, of, Subscription, switchMap } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { Curso } from 'projects/shared/models/course.model';
@@ -37,7 +37,7 @@ export class StudentListComponent {
 
   displayedColumns: string[] = [
     'displayName',
-    'contacto', 
+    'contacto',
     'department',
     'hours',
     'dates',
@@ -93,82 +93,85 @@ export class StudentListComponent {
         if (this.origen == 'admin'){
           enterpriseRef = this.enterpriseRef
           this.enterprise = await this.enterpriseService.getEnterpriseByIdPromise(enterpriseRef.id)
-        }else{
+        } else{
           enterpriseRef = this.enterpriseService.getEnterpriseRef();
           this.enterprise = this.enterpriseService.getEnterprise();
         }
         // console.log('this.enterprise',this.enterprise,enterpriseRef)
-        if(this.enterprise.examenInicial  === undefined || this.enterprise?.examenInicial){
+        if (this.enterprise.examenInicial  === undefined || this.enterprise?.examenInicial) {
           this.examenInicial = true
         }
         else{
           this.examenInicial = false
         }
         this.first = true
-        this.profileService.loadProfiles()
-        this.profilesSubscription = combineLatest([this.profileService.getProfiles$(enterpriseRef), this.departmentService.getDepartments$(enterpriseRef), this.courseService.getCourses$(enterpriseRef)]).subscribe(([profiles, departments, courses]) => {
-            this.profiles = profiles
-            this.departments = departments.sort((a, b) => a.name.localeCompare(b.name));        
-            // console.log('departments',departments)
-            this.courses = courses
-            this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe(params => {
-              let sortOrder = []
-              const page = Number(params['page']) || 1;
-              const profileFilter = params['profile'] || '';
-              this.profilefilter = profileFilter
-              const searchTerm = params['search'] || '';
-              const departmentFilter = params['iddepartment'] || '';
-              const ritmoFilter = params['ritmo'] || '';
-              const filtroUltimaActividad = params['ultActivity'] || '';
-              const sortUltimaActividad = params['sortUltimaActividad'] || '';
-              const sortRitmo = params['sortRitmo'] || '';
-              const sortRatingPoints = params['sortRatingPoints'] || '';
-              const sortDepartamento =  params['sortDepartamento'] || '';
-              const sortDatesPlan =  params['sortDatesPlan'] || '';
-              const sortHoras =  params['sortHoras'] || '';
-              const sortNombre = params['sortNombre'] || '';
-              const statusFilter = params['status'] || '';
 
-              this.ritmoFilter = ritmoFilter
-              this.filtroDepartamento = departmentFilter
-              this.filtroUltimaActividad = filtroUltimaActividad
-              this.sortUltimaActividad = sortUltimaActividad
-              this.sortRitmo = sortRitmo
-              this.sortRatingPoints = sortRatingPoints
-              this.sortDatesPlan = sortDatesPlan
-              this.sortDepartamento = sortDepartamento
-              this.sortHoras = sortHoras
-              this.sortNombre = sortNombre
-              this.statusFilter = statusFilter
+        const profiles = await firstValueFrom(this.profileService.getProfiles$(enterpriseRef))
+        const departments = await firstValueFrom(this.departmentService.getDepartments$(enterpriseRef))
+        const courses = await firstValueFrom(this.courseService.getCourses$(enterpriseRef))
 
-              // Obtener el orden de los sorts desde los parámetros de la URL
-              const urlParams = new URLSearchParams(window.location.search);
-              urlParams.forEach((value, key) => {
-                if (key === 'sortUltimaActividad' || key === 'sortRitmo' || key === 'sortRatingPoints' || key ==='sortDepartamento' || key ==='sortDatesPlan' || key === 'sortHoras' || key === 'sortNombre') {
-                  sortOrder.push({ key, value });
-                }
-              sortOrder = sortOrder.reverse();
+        this.profiles = profiles
+        this.departments = departments.sort((a, b) => a.name.localeCompare(b.name));
+        // console.log('departments',departments)
+        this.courses = courses
+        this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe(params => {
+          let sortOrder = []
+          const page = Number(params['page']) || 1;
+          const profileFilter = params['profile'] || '';
+          this.profilefilter = profileFilter
+          const searchTerm = params['search'] || '';
+          const departmentFilter = params['iddepartment'] || '';
+          const ritmoFilter = params['ritmo'] || '';
+          const filtroUltimaActividad = params['ultActivity'] || '';
+          const sortUltimaActividad = params['sortUltimaActividad'] || '';
+          const sortRitmo = params['sortRitmo'] || '';
+          const sortRatingPoints = params['sortRatingPoints'] || '';
+          const sortDepartamento =  params['sortDepartamento'] || '';
+          const sortDatesPlan =  params['sortDatesPlan'] || '';
+          const sortHoras =  params['sortHoras'] || '';
+          const sortNombre = params['sortNombre'] || '';
+          const statusFilter = params['status'] || '';
 
-              });
-              if(this.first){
-                this.performSearch(searchTerm, page, profileFilter,departmentFilter,ritmoFilter,filtroUltimaActividad,statusFilter,sortOrder);
-              }
-              else{
-                this.performSearchLocal(searchTerm, page, profileFilter,departmentFilter,ritmoFilter,filtroUltimaActividad,statusFilter,sortOrder);
-              }
-            })
+          this.ritmoFilter = ritmoFilter
+          this.filtroDepartamento = departmentFilter
+          this.filtroUltimaActividad = filtroUltimaActividad
+          this.sortUltimaActividad = sortUltimaActividad
+          this.sortRitmo = sortRitmo
+          this.sortRatingPoints = sortRatingPoints
+          this.sortDatesPlan = sortDatesPlan
+          this.sortDepartamento = sortDepartamento
+          this.sortHoras = sortHoras
+          this.sortNombre = sortNombre
+          this.statusFilter = statusFilter
+
+          // Obtener el orden de los sorts desde los parámetros de la URL
+          const urlParams = new URLSearchParams(window.location.search);
+          urlParams.forEach((value, key) => {
+            if (key === 'sortUltimaActividad' || key === 'sortRitmo' || key === 'sortRatingPoints' || key ==='sortDepartamento' || key ==='sortDatesPlan' || key === 'sortHoras' || key === 'sortNombre') {
+              sortOrder.push({ key, value });
+            }
+          sortOrder = sortOrder.reverse();
+
+          });
+          if(this.first){
+            this.performSearch(searchTerm, page, profileFilter,departmentFilter,ritmoFilter,filtroUltimaActividad,statusFilter,sortOrder);
+          }
+          else{
+            this.performSearchLocal(searchTerm, page, profileFilter,departmentFilter,ritmoFilter,filtroUltimaActividad,statusFilter,sortOrder);
+          }
         })
-        
+
       }
     })
 
   }
 
   appySort(sortOrder,users){
-
-
     if(sortOrder.length>0){
 
+
+
+      
 
       sortOrder.forEach(sort => {
 
@@ -215,7 +218,7 @@ export class StudentListComponent {
           if (sort.value === 'down') {
             users = users.sort((a, b) => b.department.localeCompare(a.department));
           }
-          
+
         }
 
         if (sort.key === 'sortRatingPoints') {
@@ -232,19 +235,19 @@ export class StudentListComponent {
 
             const activityOrder = ['low','medium','high','no iniciado','no plan'];
             users = users.sort((a, b) => {
-      
+
               // Si las fechas son iguales, ordenar por estado de actividad
               const activityStatusA = a.rhythm || '';
               const activityStatusB = b.rhythm || '';
-          
+
               const activityIndexA = activityOrder.indexOf(activityStatusA);
               const activityIndexB = activityOrder.indexOf(activityStatusB);
-          
+
               // Compara el índice de los estados de actividad en el orden invertido
               if (activityIndexA !== -1 && activityIndexB !== -1) {
                 return activityIndexA - activityIndexB;
               }
-          
+
               // Si uno de los estados no está en el array, moverlo al final
               if (activityIndexA === -1 && activityIndexB !== -1) {
                 return 1;
@@ -252,7 +255,7 @@ export class StudentListComponent {
               if (activityIndexA !== -1 && activityIndexB === -1) {
                 return -1;
               }
-          
+
               return 0; // Si ambos no están en el array, se consideran iguales en esta dimensión
             });
 
@@ -260,19 +263,19 @@ export class StudentListComponent {
           else if(sort.value == 'down'){
             const activityOrder = ['high','medium','low','no iniciado','no plan'];
             users = users.sort((a, b) => {
-      
+
               // Si las fechas son iguales, ordenar por estado de actividad
               const activityStatusA = a.rhythm || '';
               const activityStatusB = b.rhythm || '';
-          
+
               const activityIndexA = activityOrder.indexOf(activityStatusA);
               const activityIndexB = activityOrder.indexOf(activityStatusB);
-          
+
               // Compara el índice de los estados de actividad en el orden invertido
               if (activityIndexA !== -1 && activityIndexB !== -1) {
                 return activityIndexA - activityIndexB;
               }
-          
+
               // Si uno de los estados no está en el array, moverlo al final
               if (activityIndexA === -1 && activityIndexB !== -1) {
                 return 1;
@@ -280,7 +283,7 @@ export class StudentListComponent {
               if (activityIndexA !== -1 && activityIndexB === -1) {
                 return -1;
               }
-          
+
               return 0; // Si ambos no están en el array, se consideran iguales en esta dimensión
             });
 
@@ -295,16 +298,16 @@ export class StudentListComponent {
             users = users.sort((a, b) => {
               const activityStatusA = a.activityStatusText || '';
               const activityStatusB = b.activityStatusText || '';
-          
+
               // Compara el índice de los estados de actividad
               const activityIndexA = activityOrder.indexOf(activityStatusA);
               const activityIndexB = activityOrder.indexOf(activityStatusB);
-          
+
               // Si ambos estados de actividad están en el array, ordenar por índice
               if (activityIndexA !== -1 && activityIndexB !== -1) {
                 return activityIndexA - activityIndexB;
               }
-          
+
               // Si uno de los estados no está en el array, moverlo al final
               if (activityIndexA === -1 && activityIndexB !== -1) {
                 return 1;
@@ -312,11 +315,11 @@ export class StudentListComponent {
               if (activityIndexA !== -1 && activityIndexB === -1) {
                 return -1;
               }
-          
+
               // Si ambos estados de actividad no están en el array, ordenar por fecha de actividad descendente
               const dateA = a.lastActivityDate || 0;
               const dateB = b.lastActivityDate || 0;
-          
+
               return dateA - dateB;
             });
 
@@ -324,28 +327,28 @@ export class StudentListComponent {
           else if(sort.value == 'down'){
 
             const activityOrder = ['Sin clases vistas', 'Sin diagnóstico completado', 'Sin inicio sesión'];
-    
+
             users = users.sort((a, b) => {
               const dateA = a.lastActivityDate || 0;
               const dateB = b.lastActivityDate || 0;
-          
+
               // Primero ordenar por fecha de actividad descendente
               if (dateA !== dateB) {
                 return dateB - dateA;
               }
-          
+
               // Si las fechas son iguales, ordenar por estado de actividad
               const activityStatusA = a.activityStatusText || '';
               const activityStatusB = b.activityStatusText || '';
-          
+
               const activityIndexA = activityOrder.indexOf(activityStatusA);
               const activityIndexB = activityOrder.indexOf(activityStatusB);
-          
+
               // Compara el índice de los estados de actividad en el orden invertido
               if (activityIndexA !== -1 && activityIndexB !== -1) {
                 return activityIndexA - activityIndexB;
               }
-          
+
               // Si uno de los estados no está en el array, moverlo al final
               if (activityIndexA === -1 && activityIndexB !== -1) {
                 return 1;
@@ -353,7 +356,7 @@ export class StudentListComponent {
               if (activityIndexA !== -1 && activityIndexB === -1) {
                 return -1;
               }
-          
+
               return 0; // Si ambos no están en el array, se consideran iguales en esta dimensión
             });
 
@@ -734,30 +737,30 @@ export class StudentListComponent {
         }
         else if(filtroUltimaActividad =='Menos de 15 días' || filtroUltimaActividad =='Más de 30 días'){
           users = users.filter(x=>x.groupedLastActivity == filtroUltimaActividad)
-  
+
         }
         else if(filtroUltimaActividad =='Menos de 30 días'){
           users = users.filter(x=>x.groupedLastActivity == filtroUltimaActividad || x.groupedLastActivity == 'Menos de 15 días' )
-  
+
         }
         else{
           users = users.filter(x=>x.activityStatusText == filtroUltimaActividad)
-  
+
         }
       }
 
       if(statusFilter && statusFilter!='all'){
         users = users.filter(x=>x.status == statusFilter)
       }
-  
-  
+
+
       if(ritmoFilter){
         if(ritmoFilter == 'sin diagnóstico'){
           users = users.filter(user=> user.rhythm === 'no iniciado' && user.test.length==0)
         }
         else{
           users = users.filter(x=>x.rhythm == ritmoFilter)
-  
+
         }
       }
       if(departmentFilter){
@@ -765,7 +768,7 @@ export class StudentListComponent {
       }
       if (searchTerm) {
         const normalizedSearchTerm = this.removeAccents(searchTerm.toLocaleLowerCase());
-      
+
         users = users.filter(x => {
           const normalizedMail = this.removeAccents(String(x.mail).toLocaleLowerCase());
           const normalizedDisplayName = this.removeAccents(String(x.displayName).toLocaleLowerCase());
@@ -783,7 +786,7 @@ export class StudentListComponent {
       }
 
       this.appySort(sortOrder,users)
-  
+
       this.paginator.pageIndex = page - 1;
       this.dataSource.data = users;
       this.totalLength = users.length;
